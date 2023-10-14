a6a98e97ccdae9f46ad38219369e94259e6a49d2
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-10-14 17:31:03 +0100

    feat: findSentencesContaining function

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -72,7 +72,14 @@ export function getSquareRoots(nums) {
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  const arrayContainingMatchingSentences = [];
+  for (let index = 0; index < sentences.length; index++) {
+    const sentence = sentences[index];
+    if (sentence.toLowerCase().includes(str)) {
+      arrayContainingMatchingSentences.push(sentence);
+    }
+  }
+  return arrayContainingMatchingSentences;
 }
 
 export function getLongestSides(triangles) {
